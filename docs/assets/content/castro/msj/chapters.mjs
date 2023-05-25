5408f95d71c62bbd46f1c2fbf0287478864af7c6
levosro <111454197+levosro@users.noreply.github.com>
2023-05-25 13:17:25 +0300

    update

---
+++ docs/assets/content/castro/msj/chapters.mjs
@@ -0,0 +1,11 @@
+export default [
+{'title': 'Istoria îmi va da dreptate',
+'idCh': '1.01',
+},
+{'title': 'Discurs rostit la 17 februarie 1959 cu prilejul numirii în funcţia de prim ministru',
+'idCh': '1.02',
+},
+{'title': 'Cuvîntare rostită la deschiderea Congresului al X-lea al Confederației oamenilor muncii din Cuba',
+'idCh': '1.03',
+},
+];
